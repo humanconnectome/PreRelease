78c825c20c6b710bf99c0286c504f5edbbf73a4f
petra <petra.lenzini@gmail.com>
2022-12-04 11:14:34 -0600

    cc annotation, URLs, and summary stats

--- Unusables.py
+++ Unusables.py
@@ -1,5 +1,15 @@
 import pandas as pd
 import numpy as np
+import sys
+import datetime
+
+snapshotdate = datetime.datetime.today().strftime('%Y-%m-%d')
+
+
+pd.set_option('display.max_rows', 500)
+pd.set_option('display.max_columns', 500)
+pd.set_option('display.width', 10000)
+pd.set_option('display.max_colwidth',500)
 
 prepath="/home/petra/Behavioral/Lifespan/PreRelease/"
 sandyA="HCA Excluded Participants_2021May05 (1).xlsx"
@@ -94,21 +104,18 @@ PetraSandyErin['SuggestRelease3.0']=np.nan
 PetraSandyErin.loc[PetraSandyErin.Petra_IntraDB_STG.str.contains('STG')==True,'SuggestRelease3.0']=1
 PetraSandyErin=PetraSandyErin.rename(columns={'Data':'SandyWhatHappened2Data'})
 
-PetraSandyErin.loc[PetraSandyErin.subject=='HCA7297488','Petra_IntraDB_STG'] = 'CCF_HCA_STG'
-Unmergable1=pd.DataFrame([{'subject':'HCA7297488','redcap_event':'V2','Petra_IntraDB_STG':'CCF_HCA_STG','AnyUnusable':'1','PetraUnusable':'1','Petra_exclusion':'HCA7297488_DO NOT RELEASE'}])
-PetraSandyErin=pd.concat([PetraSandyErin,Unmergable1],axis=0)
-
 #COVID SPECIAL STUFF
 data={
-    0: {'subject':'HCD0099752','redcap_event': 'V1','Covid_Comeback':'X1 X2 After'},
-    1: {'subject':'HCD2604749','redcap_event': 'V1','Covid_Comeback':'X1 After'},
-    2: {'subject':'HCD0123824','redcap_event': 'V1','Covid_Comeback':'A Before'},
-    3: {'subject':'HCD2059043','redcap_event': 'V1','Covid_Comeback':'A Before'},
-    4: {'subject':'HCD2648365','redcap_event': 'V1','Covid_Comeback':'X1 After'},
-    5: {'subject':'HCD0664656','redcap_event': 'V3','Covid_Comeback':'X1 After'},
-    6: {'subject':'HCD1106728','redcap_event': 'V3','Covid_Comeback':'X1 After'},
-    7: {'subject':'HCD0541034','redcap_event': 'V3','Covid_Comeback':'X1 After'},
-    8: {'subject':'HCD2442446','redcap_event': 'V1','Covid_Comeback':'A Before, but Excluded'}
+    0: {'subject':'HCD0099752','redcap_event': 'V1','Covid_Comeback':'X1 X2 B After','ITK_URL':'https://intradb.humanconnectome.org/app/action/DisplayItemAction/search_element/xnat%3AsubjectData/search_field/xnat%3AsubjectData.ID/search_value/HCPIntradb11_S00656/project/CCF_HCD_ITK'},
+    1: {'subject':'HCD2604749','redcap_event': 'V1','Covid_Comeback':'X1 B After','ITK_URL':'https://intradb.humanconnectome.org/app/action/DisplayItemAction/search_element/xnat%3AsubjectData/search_field/xnat%3AsubjectData.ID/search_value/HCPIntradb11_S00634/popup/false/project/CCF_HCD_ITK'},
+    2: {'subject':'HCD0123824','redcap_event': 'V1','Covid_Comeback':'A Before','ITK_URL':'https://intradb.humanconnectome.org/app/action/DisplayItemAction/search_element/xnat%3AsubjectData/search_field/xnat%3AsubjectData.ID/search_value/HCPIntradb11_S00690/popup/false/project/CCF_HCD_ITK'},
+    3: {'subject':'HCD2059043','redcap_event': 'V1','Covid_Comeback':'A Before','ITK_URL':'https://intradb.humanconnectome.org/app/action/DisplayItemAction/search_element/xnat%3AsubjectData/search_field/xnat%3AsubjectData.ID/search_value/HCPIntradb11_S00734/popup/false/project/CCF_HCD_ITK'},
+    4: {'subject':'HCD2648365','redcap_event': 'V1','Covid_Comeback':'X1 B After','ITK_URL':'https://intradb.humanconnectome.org/app/action/DisplayItemAction/search_element/xnat%3AsubjectData/search_field/xnat%3AsubjectData.ID/search_value/HCPIntradb11_S00686/popup/false/project/CCF_HCD_ITK'},
+    5: {'subject':'HCD0664656','redcap_event': 'V3','Covid_Comeback':'X1 B After','ITK_URL':'https://intradb.humanconnectome.org/app/action/DisplayItemAction/search_element/xnat%3AsubjectData/search_field/xnat%3AsubjectData.ID/search_value/HCPIntradb_S05144/popup/false/project/CCF_HCD_ITK'},
+    6: {'subject':'HCD1106728','redcap_event': 'V3','Covid_Comeback':'X1 B After','ITK_URL':'https://intradb.humanconnectome.org/app/action/DisplayItemAction/search_element/xnat%3AsubjectData/search_field/xnat%3AsubjectData.ID/search_value/HCPIntradb_S05793/popup/false/project/CCF_HCD_ITK'},
+    7: {'subject':'HCD0541034','redcap_event': 'V3','Covid_Comeback':'X1 B After','ITK_URL':'https://intradb.humanconnectome.org/app/action/DisplayItemAction/search_element/xnat%3AsubjectData/search_field/xnat%3AsubjectData.ID/search_value/HCPIntradb_S06582/popup/false/project/CCF_HCD_ITK'},
+    8: {'subject':'HCD2442446','redcap_event': 'V1','Covid_Comeback':'A Before, but Excluded'},
+    9: {'subject':'HCA7626380','redcap_event': 'V1','Covid_Comeback':'X1 B After','ITK_URL':'https://intradb.humanconnectome.org/app/action/DisplayItemAction/search_element/xnat%3AsubjectData/search_field/xnat%3AsubjectData.ID/search_value/HCPIntradb11_S00656/project/CCF_HCD_ITK'}
 }
 lis = []
 
@@ -117,9 +124,62 @@ for key,val in data.items():
 Unmergable2 = pd.DataFrame(lis)
 PetraSandyErin=PetraSandyErin.merge(Unmergable2,on=['subject','redcap_event'],how='left')
 
-PetraSandyErin[['subject','redcap_event','redcap_age','PreReleaseInventory', 'Petra_IntraDB_STG', 'SuggestRelease3.0','AnyUnusable','ErinUnusable',
-                'PetraUnusable','SandyUnusable','SandyWhatData','SandyWhatHappened2Data','SandyExclusion','Petra_exclusion',
-        'Erin_exclusion','Covid_Comeback']].to_csv(prepath+'InventoryUnusables_22Nov2022.csv',index=False)
+#guy with cysts:
+#this is the guy that should be excluded.  was dnr at V1.  probably got a v2 by accident.
+PetraSandyErin.loc[PetraSandyErin.subject=='HCA7297488','Petra_IntraDB_STG'] = 'CCF_HCA_STG'
+Unmergable1=pd.DataFrame([{'subject':'HCA7297488','redcap_event':'V2','Petra_IntraDB_STG':'CCF_HCA_STG','AnyUnusable':1,'PetraUnusable':'1','Petra_exclusion':'HCA7297488_DO NOT RELEASE'}])
+PetraSandyErin=pd.concat([PetraSandyErin,Unmergable1],axis=0)
+PetraSandyErin.loc[PetraSandyErin.subject=='HCA7297488','ITK_URL'] = 'https://intradb.humanconnectome.org/app/action/DisplayItemAction/search_element/xnat%3AsubjectData/search_field/xnat%3AsubjectData.ID/search_value/HCPIntradb_S04139/popup/false/project/CCF_HCA_ITK'
+
+PetraSandyErin['Study']=''
+PetraSandyErin.loc[PetraSandyErin.subject.str.contains('HCA'),'Study']='HCA'
+PetraSandyErin.loc[PetraSandyErin.subject.str.contains('HCD'),'Study']='HCD'
+PetraSandyErin.loc[PetraSandyErin.Study=='','Study']='PCMP'
+
+PetraSandyErin[['subject','redcap_event','SuggestRelease3.0', 'Petra_IntraDB_STG', 'PreReleaseInventory', 'AnyUnusable','Covid_Comeback',
+                'Erin_exclusion','SandyExclusion','Petra_exclusion','SandyUnusable','SandyWhatData','SandyWhatHappened2Data','ErinUnusable','PetraUnusable',
+        'redcap_age','ITK_URL']].to_csv(prepath+'Lifespan_Universe_Inventory_Unusables_'+snapshotdate+'.csv',index=False)
+
+
+# Saving the reference of the standard output
+original_stdout = sys.stdout
+with open('N_Breakdown_Lifespan_Universe_'+snapshotdate+'.txt','w') as f:
+    sys.stdout = f
+    print('**********************************************')
+    print('Lifespan Universe')
+    print(PetraSandyErin.Study.value_counts(dropna=False))
+
+    print('**********************************************')
+    print('Lifespan Usables')
+    print(PetraSandyErin.AnyUnusable.value_counts(dropna=False))
+
+    print('**********************************************')
+    print('SANITY CHECK')
+    print(pd.crosstab(PetraSandyErin.Petra_IntraDB_STG,PetraSandyErin.Study))
+
+    print('**********************************************')
+    print('1st Breakdown: Exclude all subjects in Erin/Petra/Sandy Lists (including the questionable DNR), exclude PCMP')
+    print('and EXCLUDE subjects with Behavioral Data Only:')
+    for i in ['HCA','HCD']:
+        print('******************************************')
+        print(i,':')
+        subset=PetraSandyErin.loc[PetraSandyErin.Study==i]
+        print(pd.crosstab(subset['SuggestRelease3.0'], subset.redcap_event))#.to_csv('.csv',index=True)
+
+    print('**********************************************')
+    print('2nd Breakdown: Exclude all subjects in Erin/Petra/Sandy Lists (including the questionable DNR), exclude PCMP')
+    print('and INCLUDE subjects with Behavioral Data Only:')
+    subset2=PetraSandyErin.loc[(PetraSandyErin['SuggestRelease3.0']==1) | (PetraSandyErin.Petra_IntraDB_STG == "Behavioral Only")]
+    print(pd.crosstab(subset2['Study'], subset2.redcap_event))#.to_csv('.csv',index=True)
+
+    #URLs for the interesting guys
+    print('**********************************************')
+    print("URLs for the interesting guys")
+    print(PetraSandyErin.loc[PetraSandyErin.ITK_URL.isnull()==False][['subject','redcap_event','AnyUnusable','Covid_Comeback','ITK_URL']])
+    sys.stdout = original_stdout
+
+
+
 
 #'Sandy_DNR_exclusion', 'Sandy_DNR_moreinfo',
 #'Sandy_IRB_exclusion', 'Sandy_IRB_moreinfo',
